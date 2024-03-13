64c58f4662d2957cfe580b4e97b8d4df7271594b
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-13 12:37:52 -0500

    encripted users password with bcrypt dependency

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,28 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    const { username, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
     res.json({
       ok: true,
       message: "User saved successfully",
-      data: userSaved,
+      data: {
+        id: userSaved._id,
+        username: userSaved.username,
+        email: userSaved.email,
+        createAt: userSaved.createdAt,
+        updateAt: userSaved.updatedAt,
+      },
     });
   } catch (err) {
     console.log(err);
