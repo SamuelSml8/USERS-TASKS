638445455b7b29181da8bc8f549486a4032789c7
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-13 13:29:30 -0500

    finished login system

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,6 +37,49 @@ export const register = async (req, res) => {
     });
   }
 };
-export const login = (req, res) => {
-  res.send("Login");
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) {
+      return res.status(400).json({
+        ok: false,
+        message: "User not found",
+        data: null,
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        ok: false,
+        message: "Invalid password",
+        data: null,
+      });
+    }
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      ok: true,
+      message: "User logged in successfully",
+      data: {
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createAt: userFound.createdAt,
+        updateAt: userFound.updatedAt,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: err.message,
+      data: null,
+    });
+  }
 };
