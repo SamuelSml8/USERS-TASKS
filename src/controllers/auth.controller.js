81cc56495847eaa96e22cea54bd78c7447195635
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-14 09:32:54 -0500

    logout route created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -83,3 +83,10 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
