725d3dcd728e95e158f885a307953eac34f3a994
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-22 09:39:54 -0500

    Registro de usuario frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/task" element={<h1>Task page</h1>} />
         <Route path="/add-task" element={<h1>New task</h1>} />
         <Route path="/tasks/:id" element={<h1>Update page</h1>} />
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,38 @@
+import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth.js";
+
+function LoginPage() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = handleSubmit(async (values) => {
+    const res = await registerRequest(values);
+    console.log(res);
+  });
+
+  return (
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          placeholder="Password"
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
